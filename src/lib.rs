52d57ca6ded772bf79218e8b55c5b9b3a48f1140
Andrew Cassidy <drewcassidy@me.com>
2024-12-19 21:49:11 -0800

    Rework splat

--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,7 @@ use num_traits::{Bounded, One, Zero};
 use std::cmp::min;
 use std::fmt::Debug;
 use std::iter::{zip, Flatten};
-use std::ops::{Add, Index, IndexMut};
+use std::ops::{Add, Index, IndexMut, Mul};
 use std::vec::IntoIter;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
@@ -44,6 +44,17 @@ impl<T: Copy, const N: usize> From<[T; N]> for Col<T, N> {
     }
 }
 
+impl<T: Copy + One + Mul<T, Output = T>, const N: usize> One for Col<T, N>
+where
+    Col<T, N>: Splat<Col<T, N>>,
+{
+    fn one() -> Self {
+        Self {
+            data: [T::one(); N],
+        }
+    }
+}
+
 impl<T: Copy, const N: usize> IntoIterator for Col<T, N> {
     type Item = T;
     type IntoIter = <[T; N] as IntoIterator>::IntoIter;
@@ -54,6 +65,16 @@ impl<T: Copy, const N: usize> IntoIterator for Col<T, N> {
 }
 
 impl<T: Copy, const N: usize> Col<T, N> {
+    pub fn map<F: FnMut(T) -> U, U: Copy>(self, f: F) -> Col<U, N> {
+        Col::<U, N> {
+            data: self.data.map(f),
+        }
+    }
+
+    pub fn zip<F: FnMut(T, R) -> U, U: Copy, R: Copy>(self, r: Col<R, N>, mut f: F) -> Col<U, N> {
+        Col::<U, N>::try_from_rows(zip(self.rows(), r.rows()).map(|(l, r)| f(l, r))).unwrap()
+    }
+
     pub fn rows(self) -> <Self as IntoIterator>::IntoIter {
         self.into_iter()
     }
@@ -65,53 +86,165 @@ impl<T: Copy, const N: usize> Col<T, N> {
     }
 }
 
-pub trait Splat<T: Copy, const N: usize> {
-    type Scalar;
-    fn splat(self) -> Col<T, N>;
+pub trait Splat<T: Copy> {
+    fn splat(self) -> T;
 }
 
-impl<T: Copy, const N: usize> Splat<T, N> for Col<T, N> {
-    type Scalar = T;
+// impl<T: Copy + PartialOrd> Splat<T> for T {
+//     fn splat(self) -> T {
+//         self
+//     }
+// }
+
+// vector to vector identity splat
+impl<T: Copy, const N: usize> Splat<Col<T, N>> for Col<T, N> {
     fn splat(self) -> Col<T, N> {
         self
     }
 }
 
-impl<T: Copy, const N: usize> Splat<T, N> for &Col<T, N> {
-    type Scalar = T;
+// vector to vector identity splat
+impl<T: Copy, const N: usize> Splat<Col<T, N>> for &Col<T, N> {
     fn splat(self) -> Col<T, N> {
         *self
     }
 }
 
-impl<T: Copy, const N: usize> Splat<T, N> for T {
-    type Scalar = T;
+// scalar to vector splat
+impl<T: Copy, const N: usize> Splat<Col<T, N>> for T
+where
+    T: PartialOrd,
+{
     fn splat(self) -> Col<T, N> {
         Col::<T, N> { data: [self; N] }
     }
 }
 
-impl<T: Copy, const N: usize> Splat<T, N> for &T {
-    type Scalar = T;
+// scalar to vector splat
+impl<T: Copy, const N: usize> Splat<Col<T, N>> for &T
+where
+    T: PartialOrd,
+{
     fn splat(self) -> Col<T, N> {
-        Col::<T, N> { data: [*self; N] }
+        Col::<T, N> { data: [(*self); N] }
     }
 }
 
-impl<T: Copy, const W: usize, const H: usize> Splat<Col<T, W>, H> for T {
-    type Scalar = T;
-    fn splat(self) -> Col<Col<T, W>, H> {
-        Col::<Col<T, W>, H> {
-            data: [self.splat(); H],
+// vector to matrix splat
+impl<T: Copy + PartialOrd, const N: usize, const M: usize> Splat<Col<Col<T, N>, M>> for Col<T, M> {
+    fn splat(self) -> Col<Col<T, N>, M> {
+        self.map(Splat::splat)
+    }
+}
+
+// vector to matrix splat
+impl<T: Copy + PartialOrd, const N: usize, const M: usize> Splat<Col<Col<T, N>, M>> for &Col<T, M> {
+    fn splat(self) -> Col<Col<T, N>, M> {
+        self.map(Splat::splat)
+    }
+}
+
+// scalar to matrix splat
+impl<T: Copy, const N: usize, const M: usize> Splat<Col<Col<T, N>, M>> for T
+where
+    T: PartialOrd,
+{
+    fn splat(self) -> Col<Col<T, N>, M> {
+        Col::<Col<T, N>, M> {
+            data: [self.splat(); M],
         }
     }
 }
 
-impl<T: Copy, const W: usize, const H: usize> Splat<Col<T, W>, H> for &T {
-    type Scalar = T;
-    fn splat(self) -> Col<Col<T, W>, H> {
-        Col::<Col<T, W>, H> {
-            data: [self.splat(); H],
+// scalar to matrix splat
+impl<T: Copy, const N: usize, const M: usize> Splat<Col<Col<T, N>, M>> for &T
+where
+    T: PartialOrd,
+{
+    fn splat(self) -> Col<Col<T, N>, M> {
+        Col::<Col<T, N>, M> {
+            data: [(*self).splat(); M],
+        }
+    }
+}
+
+//
+// impl<T: Copy, const N: usize> Splat<Col<T, N> for Col<T, N> {
+//     type Scalar = T;
+//     fn splat(self) -> Col<T, N> {
+//         self
+//     }
+// }
+//
+// impl<T: Copy, const N: usize> Splat<T, N> for &Col<T, N> {
+//     type Scalar = T;
+//     fn splat(self) -> Col<T, N> {
+//         *self
+//     }
+// }
+//
+// impl<T: Copy, S: Copy const N : usize> Splat<T,N> for S where T : Splat<S>
+//
+// impl<T: Copy, const N: usize> Splat<T, N> for T
+// where
+//     Col<T, N>: One + Mul<T, Output = Col<T, N>>,
+// {
+//     type Scalar = T;
+//     fn splat(self) -> Col<T, N> {
+//         Col::<T, N>::one() * self
+//     }
+// }
+//
+// impl<T: Copy, const N: usize> Splat<T, N> for &T {
+//     type Scalar = T;
+//     fn splat(self) -> Col<T, N> {
+//         Col::<T, N> { data: [*self; N] }
+//     }
+// }
+
+// impl<T: Copy, const W: usize, const H: usize> Splat<Col<T, W>, H> for T {
+//     type Scalar = T;
+//     fn splat(self) -> Col<Col<T, W>, H> {
+//         Col::<Col<T, W>, H> {
+//             data: [self.splat(); H],
+//         }
+//     }
+// }
+//
+// impl<T: Copy, const W: usize, const H: usize> Splat<Col<T, W>, H> for &T {
+//     type Scalar = T;
+//     fn splat(self) -> Col<Col<T, W>, H> {
+//         Col::<Col<T, W>, H> {
+//             data: [self.splat(); H],
+//         }
+//     }
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use itertools::assert_equal;
+
+    #[test]
+    fn test_splat_scalar_to_col() {
+        let a: Col<i32, 4> = 5.splat();
+        assert_equal(a, Col::from([5, 5, 5, 5]))
+    }
+
+    #[test]
+    fn test_splat_col_to_matrix() {
+        let a = Col::from([1, 2, 3]);
+        let b: Col<Col<_, 4>, 3> = a.splat();
+        for n in 0..3 {
+            assert_equal(b.data[n], Col::from([a.data[n]; 4]))
+        }
+    }
+
+    #[test]
+    fn test_splat_scalar_to_matrix() {
+        let a: Col<Col<_, 4>, 3> = 5.splat();
+        for n in 0..3 {
+            assert_equal(a.data[n], Col::from([5; 4]));
         }
     }
 }
