1d278b959bfc4ab07232770cfab69c4b98cbe7ea
Andrew Cassidy <drewcassidy@me.com>
2024-12-31 20:10:44 -0800

    redo splatting agin

--- src/splat.rs
+++ src/splat.rs
@@ -1,23 +1,37 @@
-use crate::Col;
+use crate::{Col, Mat};
 use num_traits::Num;
+use std::num::{NonZero, Saturating, Wrapping};
 
 pub trait Scalar {}
 
-impl<T> Scalar for T where T: Num {}
+// impl<T> Scalar for T where T: Num {}
+
+macro_rules! multi_impl {
+    ($name:ident for $($t:ty),*) => ($(
+        impl $name for $t {}
+    )*)
+}
+
+multi_impl!(Scalar for i8, i16, i32, i64, i128, isize, u8, u16, u32, u64, u128, usize, f32, f64, bool);
+
+multi_impl!(Scalar for NonZero<i8>, NonZero<i16>, NonZero<i32>, NonZero<i64>, NonZero<i128>, NonZero<isize>, NonZero<u8>, NonZero<u16>, NonZero<u32>, NonZero<u64>, NonZero<u128>, NonZero<usize>);
+
+impl<T: Scalar> Scalar for Wrapping<T> {}
+impl<T: Scalar> Scalar for Saturating<T> {}
 
 pub trait Splat<T: Copy> {
     fn splat(self) -> T;
 }
 
 /// Identity splat
-impl<T: Copy + Scalar> Splat<T> for T {
+impl<T: Copy> Splat<T> for T {
     fn splat(self) -> T {
         self
     }
 }
 
 /// Identity splat (Reference)
-impl<T: Copy + Scalar> Splat<T> for &T {
+impl<T: Copy> Splat<T> for &T {
     fn splat(self) -> T {
         *self
     }
@@ -32,10 +46,19 @@ impl<T: Copy, R: Scalar + Splat<T>, const N: usize> Splat<Col<T, N>> for R {
     }
 }
 
-/// Vector to Vector splat
-impl<T: Copy, R: Copy + Splat<T>, const N: usize> Splat<Col<T, N>> for Col<R, N> {
-    fn splat(self) -> Col<T, N> {
-        self.map(Splat::splat)
+/// Vector to Matrix Splat
+impl<T: Copy + Splat<Col<T, W>>, const H: usize, const W: usize> Splat<Mat<T, H, W>> for Col<T, H> {
+    fn splat(self) -> Mat<T, H, W> {
+        self.map(|row| row.splat())
+    }
+}
+
+/// Vector to Matrix Splat (Reference)
+impl<T: Copy + Splat<Col<T, W>>, const H: usize, const W: usize> Splat<Mat<T, H, W>>
+    for &Col<T, H>
+{
+    fn splat(self) -> Mat<T, H, W> {
+        self.map(|row| row.splat())
     }
 }
 
